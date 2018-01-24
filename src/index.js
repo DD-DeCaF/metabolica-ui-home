eb5f274908acc829d2c704d2bd3123b05ee0eb8c
Ali Kaafarani <ali@kvikshaug.no>
2018-01-24 16:54:53 +0100

    fix: apply metabolica AppModule, not DevAppModule

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import angular from 'angular';
-import {DevAppModule} from 'metabolica';
+import {AppModule} from 'metabolica';
 import {HomeModule} from './home/home.module';
 export {HomeModule} from './home/home.module';
 
 export const HomeAppModule = angular.module('HomeApp', [
-  DevAppModule.name,
+  AppModule.name,
   HomeModule.name
 ]);
